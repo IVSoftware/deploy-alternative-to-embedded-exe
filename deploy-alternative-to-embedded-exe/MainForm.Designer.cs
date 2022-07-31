1bcf6dcb4e01f440e12f1a359f4f0e645be58ec8
Tom Gregor <tgregor@ivsoftware.com>
2022-07-31 16:20:45 -0600

    Add project files.

---
+++ deploy-alternative-to-embedded-exe/MainForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace read_stream_embedded_exe_onclick
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btScreenshot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btScreenshot
+            // 
+            this.btScreenshot.Location = new System.Drawing.Point(39, 79);
+            this.btScreenshot.Name = "btScreenshot";
+            this.btScreenshot.Size = new System.Drawing.Size(112, 34);
+            this.btScreenshot.TabIndex = 0;
+            this.btScreenshot.Text = "Screenshot";
+            this.btScreenshot.UseVisualStyleBackColor = true;
+            this.btScreenshot.Click += new System.EventHandler(this.btScreenshot_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 244);
+            this.Controls.Add(this.btScreenshot);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btScreenshot;
+    }
+}
+
---
+++ deploy-alternative-to-embedded-exe/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace read_stream_embedded_exe_onclick
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void btScreenshot_Click(object sender, EventArgs e)
+        {
+            string sourceFileName = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Properties",
+                "Resources",
+                "AndroidBSPInstaller",
+                "adb.exe"
+            );
+
+            string exePath = "c:\\tmp\\adb.exe";
+            var exeDir = Path.GetDirectoryName(exePath);
+
+            Directory.CreateDirectory(exeDir);
+            File.Copy(sourceFileName, exePath, overwrite: true);
+
+            // Open the folder to see the result
+            Process.Start("explorer.exe", Path.GetDirectoryName(exePath));
+        }
+    }
+}
